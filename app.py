022c216a66064c1a8cf1de620ecf223324e0c543
David Toomer <djtoomer22@gmail.com>
2020-07-26 02:27:41 -0400

    add main app

---
+++ app.py
@@ -0,0 +1,6 @@
+from gui.windows.GWindow import GWindow
+
+if __name__ == '__main__':
+  root = GWindow()
+  root.title('Impact Visualizer')
+  root.mainloop()
--- gui/components/GSection.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from gui.constants import *
-
-class GSection(tk.Frame):
-  def __init__(self, master = None, *args, **kwargs):
-    tk.Frame.__init__(self, master, *args, **kwargs)
-
-    self.configure(padx = PAGE_PADX, pady = PAGE_PADY)
