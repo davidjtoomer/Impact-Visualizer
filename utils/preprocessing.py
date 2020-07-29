d20ae095079a44b591ef0bc54debadb5b34796e2
David Toomer <djtoomer22@gmail.com>
2020-07-29 03:22:31 -0400

    add callback helper modules

---
+++ utils/preprocessing.py
@@ -0,0 +1,11 @@
+import io
+import base64
+import pandas as pd
+
+def parse_impact(contents):
+  datafiles = []
+  for content in contents:
+    _, content_string = content.split(',')
+    decoded = base64.b64decode(content_string)
+    datafiles.append(pd.read_excel(io.BytesIO(decoded), sheet_name = 'DATA', header = None, nrows = 6000, usecols = 'E:F', keep_default_na = False))
+  return datafiles
