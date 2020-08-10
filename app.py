9ec7e74c2a701be6a7ac05c9375bbe82f2fb80d0
David Toomer <djtoomer22@gmail.com>
2020-08-10 08:35:15 -0400

    edit callbacks, move graph buttons to sidebar

--- app.py
+++ app.py
@@ -30,13 +30,15 @@ app.layout = html.Div(
       # main instructions
       className = 'four columns instruction',
       children = [
-        html.H1(children = 'Impact Visualizer'),
+        html.H1('Impact Visualizer'),
         html.P(
           '''
           - View source code (GitHub) by clicking "SOURCE"
           - Upload impact files by clicking "UPLOAD DATA"
           - Upload new impact files by clicking "UPLOAD DATA" again
-          '''
+          - If your files are not named by their y-position, you must also include the MATLAB script (plot.m or Surface_Plot.m) containing the ystep variable.
+          ''',
+          className = 'instruction-text'
         ),
         # source and upload buttons
         html.Div(
@@ -48,6 +50,7 @@ app.layout = html.Div(
               children = [
                 dcc.Link(
                   href = 'https://github.com/davidjtoomer/Impact-Visualizer',
+                  target = '_blank',
                   children = [
                     html.Button(
                       'SOURCE',
@@ -62,7 +65,7 @@ app.layout = html.Div(
             dcc.Upload(
               id = 'upload-data',
               multiple = True,
-              className='mobile-button',
+              className = 'mobile-button',
               children = [
                 html.Button(
                   'UPLOAD DATA',
@@ -73,6 +76,8 @@ app.layout = html.Div(
             )
           ]
         ),
+        # empty: impact toolbar
+        html.Div(id = 'impact-toolbar'),
         # empty: scanline toolbar
         html.Div(id = 'scanline-toolbar')
       ]
@@ -94,8 +99,9 @@ app.layout = html.Div(
 @app.callback( # make graphs visible upon file submission
   [
     Output('impact-graph-container', 'children'),
-    Output('scanline-toolbar', 'children'),
-    Output('scanline-graph-container', 'children')
+    Output('impact-toolbar', 'children'),
+    Output('scanline-graph-container', 'children'),
+    Output('scanline-toolbar', 'children')
   ],
   [
     Input('upload-data', 'contents'),
@@ -108,8 +114,10 @@ def load_data(contents, filenames):
     ystep = preprocessing.extract_ystep(matlab_contents)
     datafiles = preprocessing.parse_impact(contents)
     global impact 
+    global figure
     impact = Impact(datafiles, filenames, ystep)
-    impact_figure = callbacks.impact_figure(impact)
+    figure, impact_figure = callbacks.impact_figure(impact)
+    impact_toolbar = callbacks.impact_toolbar(impact)
     scanline_toolbar = callbacks.scanline_toolbar(impact)
 
     scanline_button_divs = []
@@ -121,8 +129,8 @@ def load_data(contents, filenames):
         )
       ))
     
-    return impact_figure, scanline_toolbar, scanline_button_divs
-  return None, None, None
+    return impact_figure, impact_toolbar, scanline_button_divs, scanline_toolbar 
+  return None, None, None, None
 
 @app.callback(
   Output(dict(
@@ -146,6 +154,76 @@ def display_scanline(n_clicks, scanline_id):
   if n_clicks and n_clicks % 2:
     return callbacks.scanline_figure(impact, scanline_id['index'])
   return None
+
+
+@app.callback(
+  Output('impact-graph', 'figure'),
+  [
+    Input('num-contours-slider', 'value'),
+    Input('colorscale-dropdown', 'value'),
+    Input('projection-dropdown', 'value'),
+    Input('view-dropdown', 'value'),
+    Input('x-range-slider', 'value'),
+    Input('y-range-slider', 'value'),
+    Input('z-scale-slider', 'value')
+  ]
+)
+def update_impact_graph(num_contours, colorscale, projection, view, x_range, y_range, z_scale):
+  return callbacks.update_impact(impact, figure, num_contours, colorscale, projection, view, x_range, y_range, z_scale)
+
+@app.callback(
+  Output('display-num-contours', 'children'),
+  [
+    Input('num-contours-slider', 'value')
+  ]
+)
+def display_num_contours(num_contours):
+  return [
+    html.P(f'{num_contours}')
+  ]
+
+@app.callback(
+  [
+    Output('left-x-range-slider', 'children'),
+    Output('right-x-range-slider', 'children')
+  ],
+  [
+    Input('x-range-slider', 'value')
+  ]
+)
+def display_xrange(value):
+  return [
+    html.P(f'{value[0]:.3f}')
+  ], [
+    html.P(f'{value[1]:.3f}')
+  ]
+
+@app.callback(
+  [
+    Output('left-y-range-slider', 'children'),
+    Output('right-y-range-slider', 'children')
+  ],
+  [
+    Input('y-range-slider', 'value')
+  ]
+)
+def display_yrange(value):
+  return [
+    html.P(f'{value[0]:.3f}')
+  ], [
+    html.P(f'{value[1]:.3f}')
+  ]
+
+@app.callback(
+  Output('display-z-scale', 'children'),
+  [
+    Input('z-scale-slider', 'value')
+  ]
+)
+def display_zscale(value):
+  return [
+    html.P(f'{(10 ** value):.3f}')
+  ]
 
 # run the server
 if __name__ == '__main__':
